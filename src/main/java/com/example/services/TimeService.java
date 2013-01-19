852bd0249f0f4c738ce75591ebf4cb4b85fcacc4
William Zhang <wjzhang92@gmail.com>
2013-01-19 13:11:33 -0500

    test

--- src/main/java/com/example/services/TimeService.java
+++ src/main/java/com/example/services/TimeService.java
@@ -4,7 +4,11 @@ import java.util.Set;
 
 import com.example.Main;
 import com.example.models.Time;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -18,17 +22,24 @@ public class TimeService {
     @GET
     public String get() {
     	
-    	DB mdb = Main.mongo;
+    	DB db = Main.mongo;
     	
-    	Set<String> collections = mdb.getCollectionNames();
+    	DBCollection coll = db.getCollection("profile");
+    	DBObject element = coll.findOne();
     	
-    	String coll = "";
-    	
-    	for (String c: collections) {
-    		coll += c;
+    	BasicDBObject doc = new BasicDBObject ("_firstname","William").
+    									append("_lastname","Zhang").
+    									append("_contact","8583665371");
+    	coll.insert(doc);
+    	DBCursor cursor = coll.find();
+    	try{
+    		while(cursor.hasNext()){
+    			System.out.println(cursor.next());
+    		}
+    	} finally{
+    		cursor.close();
     	}
-    	
-        return coll;
+    	return "";
     }
 
 }
