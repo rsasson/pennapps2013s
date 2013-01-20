9a9e654ca52809e9648d4b1e0c0015d1c4827f6e
Ryan Sasson <ryan.sasson@gmail.com>
2013-01-19 22:30:18 -0500

    next bug fix

--- src/main/java/com/pennapps/services/AppService.java
+++ src/main/java/com/pennapps/services/AppService.java
@@ -19,6 +19,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@Path("pennapps/")
 @Produces(MediaType.APPLICATION_JSON)
 public class AppService {
 
