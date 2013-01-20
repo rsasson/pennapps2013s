4e8ef3fabc7bb0dde17ae858b37dc99f11d6d6da
Ryan Sasson <ryan.sasson@gmail.com>
2013-01-19 21:36:13 -0500

    simplifying path

--- src/main/java/com/pennapps/services/AppService.java
+++ src/main/java/com/pennapps/services/AppService.java
@@ -19,7 +19,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/test")
 @Produces(MediaType.APPLICATION_JSON)
 public class AppService {
 
